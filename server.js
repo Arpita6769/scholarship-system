74e12224e48d161f9ef2b46c0703f2014c0393ca
Arpita Halwasia <arpitahalwasia@gmail.com>
2026-02-18 18:30:12 +0530

    Added bcrypt and jsonwebtiken dependenies

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const db = require("./db");
 
 const app = express();
 const PORT = 3000;
@@ -7,6 +8,17 @@ app.get("/", (req, res) => {
   res.send("Scholarship Management System Running 🚀");
 });
 
+app.get("/test-db", (req, res) => {
+  db.query("SELECT 1", (err, result) => {
+    if (err) {
+      return res.status(500).send("Database error");
+    }
+    res.send("Database connected successfully ✅");
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
